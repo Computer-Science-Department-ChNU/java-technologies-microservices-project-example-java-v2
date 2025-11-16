6bbc6c4819b09f0903d6d6a791d79439b59c7b10
Edgar Vatamanitsa <e.vatamanitsa@chnu.edu.ua>
2025-11-16 20:07:45 +0200

    [13] Implemented the REST data approach for reservation service.

---
+++ reservation-service/src/main/java/ua/edu/chnu/kkn/reservation/rest/ReservationCrudResource.java
@@ -0,0 +1,9 @@
+package ua.edu.chnu.kkn.reservation.rest;
+
+import io.quarkus.hibernate.orm.rest.data.panache.PanacheEntityResource;
+import io.quarkus.rest.data.panache.ResourceProperties;
+import ua.edu.chnu.kkn.reservation.reservation.entity.Reservation;
+
+@ResourceProperties(path = "/admin/reservation")
+public interface ReservationCrudResource extends PanacheEntityResource<Reservation, Long> {
+}
